2b4cc8bc4563115ca8b714eb679e049b0c0465c2
niooii <wangh3@bxscience.edu>
2024-02-16 01:31:39 -0500

    interior mutability something something

--- sdl2-app/src/app.rs
+++ sdl2-app/src/app.rs
@@ -1,17 +1,24 @@
-
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 use crate::error::Result;
-
 use crate::render_components::RenderComponent;
-use sdl2::{self, image::{self, InitFlag, LoadTexture, Sdl2ImageContext}, pixels::Color, render::{Canvas, CanvasBuilder, Texture, TextureCreator}, video::{Window, WindowBuilder, WindowContext}, EventPump, Sdl, VideoSubsystem};
+
+use sdl2::{
+    self,
+    image::{self, InitFlag, LoadTexture, Sdl2ImageContext},
+    pixels::Color,
+    render::{Canvas, Texture, TextureCreator},
+    video::{Window, WindowBuilder, WindowContext},
+    EventPump, Sdl, VideoSubsystem,
+};
 use sdl2::event::Event;
 
 #[derive(PartialEq, Eq)]
 pub enum AppState {
     Ok,
     RequestedQuit,
-    LowMemory
+    LowMemory,
 }
 
 pub struct App<'a> {
@@ -19,13 +26,13 @@ pub struct App<'a> {
     sdl2_image_instance: Sdl2ImageContext,
     video_subsystem: VideoSubsystem,
     texture_creator: TextureCreator<WindowContext>,
-    canvas: Canvas<Window>,
-    event_pump: EventPump,
+    canvas: Arc<Mutex<Canvas<Window>>>,
+    event_pump: Arc<Mutex<EventPump>>,
 
     // internals
     clear_color: Color,
     render_components: HashMap<String, RenderComponent>,
-    texture_map: HashMap<String, Texture<'a>>,
+    texture_map: Arc<Mutex<HashMap<String, Texture<'a>>>>,
 }
 
 impl<'a> App<'a> {
@@ -34,46 +41,42 @@ impl<'a> App<'a> {
         let video_subsystem = sdl2_instance.video()?;
         let sdl2_image_instance = sdl2::image::init(InitFlag::all())?;
         let window = WindowBuilder::new(&video_subsystem, name, w, h)
-        .allow_highdpi()
-        .opengl()
-        .build().map_err(|e| e.to_string())?;
+            .allow_highdpi()
+            .opengl()
+            .build()
+            .map_err(|e| e.to_string())?;
 
-        let canvas = window
-        .into_canvas()
-        .accelerated()
-        .build()
-        .map_err(|e| e.to_string())?;
+        let canvas = window.into_canvas().accelerated().build().map_err(|e| e.to_string())?;
 
-        let event_pump = sdl2_instance.event_pump()?;
-        
-        Ok(
-            Self {
-                sdl2_instance,
-                sdl2_image_instance,
-                video_subsystem, 
-                texture_creator: canvas.texture_creator(),
-                canvas,
-                event_pump,
-                
-                clear_color: Color::BLACK,
-                render_components: HashMap::new(),
-                texture_map: HashMap::new()
-            }
-        )
+        let event_pump = Arc::new(Mutex::new(sdl2_instance.event_pump()?));
+        let texture_creator = canvas.texture_creator();
+        let texture_map = Arc::new(Mutex::new(HashMap::new()));
 
+        let canvas = Arc::new(Mutex::new(canvas));
+
+        Ok(Self {
+            sdl2_instance,
+            sdl2_image_instance,
+            video_subsystem,
+            canvas,
+            texture_creator,
+            event_pump,
+            clear_color: Color::BLACK,
+            render_components: HashMap::new(),
+            texture_map,
+        })
     }
-}
 
-impl<'a> App<'a> {
-    pub fn draw_frame<'b>(&'a mut self) -> AppState 
-    where 'a: 'b
-    {
-        // events are auto handled, maybe change later if i need it for any reason
+    pub fn draw_frame(&'a self) -> AppState {
+        // events are auto-handled, maybe change later if I need it for any reason
+        let mut canvas = self.canvas.lock().unwrap();
+        let mut event_pump = self.event_pump.lock().unwrap();
+        let mut texture_map = self.texture_map.lock().unwrap();
 
-        for event in self.event_pump.poll_iter() {
+        for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. } => return AppState::RequestedQuit,
-                Event::MouseButtonDown { which, mouse_btn, clicks, x, y, .. } => {
+                Event::MouseButtonDown { .. } => {
                     for (_key, component) in &self.render_components {
                         // todo! check click and component bounds
                         if let Some(callback) = &component.on_click_fn() {
@@ -81,33 +84,32 @@ impl<'a> App<'a> {
                             print!("called callback fn");
                         }
                     }
-                },
-                _ => continue
+                }
+                _ => continue,
             }
         }
 
-        self.canvas.set_draw_color(self.clear_color);
-        self.canvas.clear();
+        canvas.set_draw_color(self.clear_color);
+        canvas.clear();
 
         // render the components
         for (_key, component) in self.render_components.iter() {
-            if !self.texture_map.contains_key(component.texture_path()) {
-                self.texture_map.insert(
-                    component.texture_path().clone(), 
-                    self.texture_creator.load_texture(
-                        component.texture_path()
-                    ).expect("Failed to load texture")
+            if !texture_map.contains_key(component.texture_path()) {
+                texture_map.insert(
+                    component.texture_path().clone(),
+                    self.texture_creator
+                        .load_texture(component.texture_path())
+                        .expect("Failed to load texture"),
                 );
             }
         }
 
-        self.canvas.present();
+        canvas.present();
 
         AppState::Ok
     }
 
     pub fn add_component(&mut self, unique_id: &str, render_component: RenderComponent) -> Result<()> {
-        
         let id = unique_id.to_string();
 
         if self.render_components.contains_key(&id) {
@@ -127,12 +129,5 @@ impl<'a> App<'a> {
 
     pub fn set_clear_color(&mut self, r: u8, g: u8, b: u8, a: u8) {
         self.clear_color = Color::RGBA(r, g, b, a);
-    }
-
-}
-
-impl Drop for App<'_> {
-    fn drop(&mut self) {
-        self.render_components.clear();
     }
 }
